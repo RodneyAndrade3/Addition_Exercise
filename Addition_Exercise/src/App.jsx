4721eddbf6fc271568b1cab98366c97ea942b892
RodneyAndrade3 <120497251+RodneyAndrade3@users.noreply.github.com>
2026-04-23 17:20:23 -0500

    calculator

--- Addition_Exercise/src/App.jsx
+++ Addition_Exercise/src/App.jsx
@@ -1,43 +1,99 @@
-import { useState } from 'react'
-
-
-function App() {
-  
-  const [number1, setNumber1] = useState('')
-  const [number2, setNumber2] = useState('')
+import { useState } from "react";
+import "./App.css";
+function app() {
+  const [number1, setNumber1] = useState("");
+  const [number2, setNumber2] = useState("");
+  const [vista, setVista] = useState("");
+  const [calculate, setcalculate] = useState("");
 
   const sumar = () => {
-    // Convertimos a número y sumamos
-    const resultado = Number(number1) + Number(number2)
-    alert(`El resultado es: ${resultado}`)
-  }
+    const resultado = Number(number1) + Number(number2);
+    alert(`El resultado es: ${resultado}`);
+  };
+
+  const calculator = value => {
+    if (value === "C") {
+      setcalculate("");
+    } else if (value === "Remove") {
+      setcalculate(calculate.slice(0, -1));
+    } else if (value === "=") {
+      try {
+        setcalculate(eval(calculate).toString());
+      } catch (error) {
+        alert("Error en la expresión");
+      }
+    } else {
+      setcalculate(calculate + value);
+    }
+  };
 
   return (
-    <div >
-      <h3>Addition Exercise</h3>
-      
-      <input 
-        type="text" 
-        placeholder="Número 1" 
-        value={number1}
-        onChange={(e) => setNumber1(e.target.value)} 
-      />
+    <>
+      <h2>Ejercicios</h2>
+      <br />
+      <button onClick={() => setVista("suma")}>Ejercicio Suma </button>
       <br />
       <br />
+      <button onClick={() => setVista("calculadora")}>Calculadora</button>
+      {vista === "suma" && (
+        <div>
+          <h3>Ingrese dos números para sumarlos</h3>
 
-      <input 
-        type="text" 
-        placeholder="Número 2" 
-        value={number2}
-        onChange={(e) => setNumber2(e.target.value)} 
-      />
-      <br />
-      <br />
+          <input
+            type="text"
+            placeholder="Numero 1"
+            value={number1}
+            onChange={e => setNumber1(e.target.value)}
+          />
+          <br />
+          <br />
 
-      <button onClick={sumar}>Sumar</button>
-    </div>
-  )
+          <input
+            type="text"
+            placeholder="Numero 2"
+            value={number2}
+            onChange={e => setNumber2(e.target.value)}
+          />
+          <br />
+          <br />
+
+          <button onClick={sumar}>Sumar</button>
+        </div>
+      )}
+      {vista === "calculadora" && (
+        <>
+          <h3>Calculadora</h3>
+          <div className="contenedor">
+            <input
+              className="pantalla"
+              type="text"
+              placeholder="Ingrese un numero"
+              value={calculate}
+            />
+
+            <div className="botones-calculadora">
+            
+              {[
+                "C", "/",  "*", "Remove",
+                "7", "8",  "9",  "-",
+                "4", "5",  "6",  "+",
+                "1", "2",  "3",  "=",
+                "0",       ".",
+              ].map(value => (
+                <button key={value} onClick={() => calculator(value)}>
+                  {value}
+                </button>
+              ))}
+            </div>
+            <br />
+            <br />
+          </div>
+       </>
+      )}
+    </>
+  );
 }
 
-export default App
+export default app;
+
 
